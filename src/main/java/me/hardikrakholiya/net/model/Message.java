ca4b7eb887c22476ffdb9c561507ee358b9beab8
Hardik Rakholiya <hmrakholiya@gmail.com>
2019-03-18 21:27:53 -0400

    fixed an insidious bug in parsing message ids

--- src/main/java/me/hardikrakholiya/net/model/Message.java
+++ src/main/java/me/hardikrakholiya/net/model/Message.java
@@ -1,6 +1,7 @@
 package me.hardikrakholiya.net.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class Message implements Serializable, Comparable<Message> {
     private final String id;
@@ -36,19 +37,19 @@ public class Message implements Serializable, Comparable<Message> {
 
     @Override
     public int compareTo(Message other) {
-//        Integer[] clockProcessIdPair = (Integer[]) Arrays.stream(id.split(".")).map(Integer::parseInt).toArray();
-//        Integer[] otherClockProcessIdPair = (Integer[]) Arrays.stream(other.getId().split(".")).map(Integer::parseInt).toArray();
-//        int clock = clockProcessIdPair[0];
-//        int processId = clockProcessIdPair[1];
-//        int otherClock = otherClockProcessIdPair[0];
-//        int otherProcessId = otherClockProcessIdPair[1];
-//
-//        if (clock != otherClock) {
-//            return Integer.compare(clock, otherClock);
-//        }
-//
-//        return Integer.compare(processId, otherProcessId);
+        Integer[] clockProcessIdPair = Arrays.stream(id.split("\\.")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] otherClockProcessIdPair = Arrays.stream(other.getId().split("\\.")).map(Integer::parseInt).toArray(Integer[]::new);
 
-        return Double.compare(new Double(this.id), new Double(other.id));
+        int clock = clockProcessIdPair[0];
+        int otherClock = otherClockProcessIdPair[0];
+
+        if (clock != otherClock) {
+            return Integer.compare(clock, otherClock);
+        }
+
+        int processId = clockProcessIdPair[1];
+        int otherProcessId = otherClockProcessIdPair[1];
+
+        return Integer.compare(processId, otherProcessId);
     }
 }
